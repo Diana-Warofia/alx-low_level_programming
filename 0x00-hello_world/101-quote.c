565467f782fff2743ab50042d1d5a6f56df6bc2a
Diana-Warofia <125636060+Diana-Warofia@users.noreply.github.com>
2023-03-17 23:30:33 +0000

    Size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19
- * Return: 1 (Success)
- */
+ *  * main - prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19
+ *   * Return: 1 (Success)
+ *    */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout):
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
