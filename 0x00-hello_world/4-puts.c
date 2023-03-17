739f1989ae292d81f3e7f0a264c0b5f4ca10b179
Diana-Warofia <125636060+Diana-Warofia@users.noreply.github.com>
2023-03-17 22:25:44 +0000

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - This program uses puts function instead of printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
