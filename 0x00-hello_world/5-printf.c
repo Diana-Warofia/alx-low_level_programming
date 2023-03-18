9eec5b938dcb4f51f6e149940ca2655e091749a3
Diana-Warofia <125636060+Diana-Warofia@users.noreply.github.com>
2023-03-18 22:19:07 +0000

    Print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types on the computer
+ * main - This program uses printf instead of puts.
  * Return: 0 (Success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int  li;
-	long long int lli;
-	float f;
-
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printof("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long ong int: %lu byte(s)\n" (unsigned long)sizeof(lli));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
