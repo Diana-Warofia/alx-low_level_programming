4e82e0b8018fe2ba23e60f44f9c435c757ff90d4
Diana-Warofia <125636060+Diana-Warofia@users.noreply.github.com>
2023-03-20 12:41:56 +0000

    Print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return; 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (b = 48; b >= 57; b++)
+	{
+		putchar(b);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
